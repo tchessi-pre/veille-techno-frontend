53099571ce6d3a60014c3200ea102fff29ebc95c
Tchess <tchesspreza@yahoo.fr>
2023-11-15 11:00:48 +0100

    Add task modal

--- my-todo-list/src/app/add-task-modal/add-task-modal.component.ts
+++ my-todo-list/src/app/add-task-modal/add-task-modal.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-task-modal.component.html',
-  styleUrl: './add-task-modal.component.css'
+  styleUrls: ['./add-task-modal.component.css'],
 })
 export class AddTaskModalComponent {
+  taskTitle: string = '';
+  taskDescription: string = '';
+  showAddTaskModal = false;
+  @Input() showModal: boolean = false; // Garder cette propriété
+  @Output() taskAdded = new EventEmitter<{
+    title: string;
+    description: string;
+  }>();
 
+  openModal() {
+    this.showAddTaskModal = true;
+  }
+  submitTask(): void {
+    if (this.taskTitle.trim()) {
+      // Envoyer les données de la tâche au composant parent
+      this.taskAdded.emit({
+        title: this.taskTitle,
+        description: this.taskDescription,
+      });
+      this.closeModal();
+    }
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
 }
--- my-todo-list/src/app/task-list/task-list.component.ts
+++ my-todo-list/src/app/task-list/task-list.component.ts
@@ -8,15 +8,37 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { AddTaskModalComponent } from '../add-task-modal/add-task-modal.component';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [CommonModule, CdkDropListGroup, CdkDropList, CdkDrag],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css',
+  imports: [
+    CommonModule,
+    CdkDropListGroup,
+    CdkDropList,
+    CdkDrag,
+    AddTaskModalComponent,
+  ],
 })
 export class TaskListComponent {
+openModal() {
+  this.showAddTaskModal = true;
+}
+  showAddTaskModal = false;
+
+  oopenModal() {
+    this.showAddTaskModal = true;
+  }
+
+  onTaskAdded(event: { title: string; description: string }) {
+    // Ajoutez ici votre logique pour gérer l'ajout de la nouvelle tâche
+    // Par exemple, ajouter la tâche à la liste 'todo'
+    this.todo.push(event.title);
+  }
+
   todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
   done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
