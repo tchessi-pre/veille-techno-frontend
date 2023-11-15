60d3dd1fb6183b465580eb1dcf532027bb0db29d
Tchess <tchesspreza@yahoo.fr>
2023-11-15 09:28:24 +0100

    Add task list style

---
+++ my-todo-list/src/app/add-task-modal/add-task-modal.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-task-modal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-task-modal.component.html',
+  styleUrl: './add-task-modal.component.css'
+})
+export class AddTaskModalComponent {
+
+}
