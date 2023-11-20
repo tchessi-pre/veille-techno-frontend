f5b46218c685d6f33d123f4b9a3868eb85810ff2
Tchess <tchesspreza@yahoo.fr>
2023-11-20 09:14:47 +0100

    fix error

---
+++ kanban-board-app/src/app/kanban-board/kanban-board.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-kanban-board',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './kanban-board.component.html',
+  styleUrl: './kanban-board.component.css'
+})
+export class KanbanBoardComponent {
+
+}
